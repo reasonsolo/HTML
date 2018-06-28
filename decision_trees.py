c52995f36563552e863fea761166108d4f6ea3a8
ZHOU Lizhi <zhoulzh@rongyi.com>
2018-06-28 17:37:17 +0800

    reading about decision trues

---
+++ decision_trees.py
@@ -0,0 +1,10 @@
+
+class RegressionTree():
+    # a good example
+    # ref http://www.saedsayad.com/decision_tree_reg.htm
+    pass
+
+class ClassifyTree():
+    # example
+    # ref http://www.saedsayad.com/decision_tree.htm
+    pass
